0727abda65aba70412a07007d46e87f0e1fb9549
Mehdi Rostamzadeh <mehdi.rstmzdh@gmail.com>
2026-08-25 16:46:37 +0330

    Final data preprocessing

--- src/data_prep.py
+++ src/data_prep.py
@@ -56,11 +56,21 @@ def prepare_data():
         stratify=y,
         random_state=42
     )
+    
+    X_train_raw , X_test_raw , y_train , y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        stratify=y,
+        random_state=42
+    )
 
+    
+    
 
     scaler = StandardScaler()
 
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
 
-    return X_train , X_test , y_train , y_test , scaler
+    return X_train , X_test , y_train , y_test , scaler , X_train_raw , X_test_raw
